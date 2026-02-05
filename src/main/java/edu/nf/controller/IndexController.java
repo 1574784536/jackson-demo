2be32d8f6b191c90482258cccddcfd326a0bd833
17520275940 <>
2026-02-05 14:35:14 +0800

    test vo2

--- src/main/java/edu/nf/controller/IndexController.java
+++ src/main/java/edu/nf/controller/IndexController.java
@@ -1,5 +1,6 @@
 package edu.nf.controller;
 
+import edu.nf.entity.TestVo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,9 @@ public class IndexController {
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index() {
+        TestVo vo = new TestVo();
+        vo.setAddress("123");
+        vo.setName("345");
         return "index";
     }
 }
